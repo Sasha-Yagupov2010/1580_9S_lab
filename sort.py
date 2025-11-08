aa82ec6cdb6e05a3a6fca6956e470a4ae3952b06
Sasha <sasha.yagupov1401@mail.ru>
2025-11-08 21:32:45 +0300

    наброски

--- sort.py
+++ sort.py
@@ -5,21 +5,79 @@ def generate_random_count(from_count, to_count):
     return random_number
 
 def generate_random_list(min_count, max_count, list_length):
-    arr = [random.randint(min_count, max_count) for _ in range(list_length)]
+    arr = [random.randint(min_count, max_count) for i in range(list_length)]
     return arr
 
 def selection_sort(arr):
-    n = len(arr)
-    for i in range(n):
+    arr_length = len(arr)
+    for i in range(arr_length):
         min_idx = i
-        for j in range(i + 1, n):
+        for j in range(i + 1, arr_length):
             if arr[j] < arr[min_idx]:
                 min_idx = j
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
     return arr
 
+
+def bubble_sort(arr):
+    arr_length = len(arr)
+    for i in range(arr_length):
+        for j in range(0, arr_length - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+
+
+def demo_mode():
+    print("введите диапозон значений для заполнения")
+    print("формат: мин.знач  макс.знач  длинна")
+    input_line = input().split()
+    if len(input_line) == 3:
+        if input_line[0].isdigit() and input_line[1].isdigit()and input_line[2].isdigit():
+            min_count = int(input_line[0])
+            max_count = int(input_line[1])
+            arr_length = int(input_line[2])
+
+            arr = generate_random_list(min_count, max_count,arr_length)
+            print("созданный массив:")
+            print(*arr)
+
+        else:
+            print("ошибка, неверные входные данные!")
+            return
+    else:
+        print("ошибка, неверные входные данные!")
+        return
+
+def interactive_mode():
+    print("")
+
+
 def main():
-    pass
+    global_run = True
+    '''menu'''
+    while global_run:
+        print("1 - демонстрация")
+        print("2 - интерактивный")
+        print("3 - выход")
+
+        input_line = input().split()
+        if len(input_line) == 1:
+            tmp = input_line[0]
+            if tmp.isdigit():
+                tmp = int(tmp)
+
+                if tmp == 1:
+                    demo_mode()
+                elif tmp == 2:
+                    interactive_mode()
+                else:
+                    global_run = False
+            else:
+                print("ошибка!")
+
+
 
 
 if __name__ == "__main__":
