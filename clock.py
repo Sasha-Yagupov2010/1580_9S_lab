a364495c5d463e2e04cbce12fa1e76fc7d227597
Sasha <sasha.yagupov1401@mail.ru>
2025-11-08 17:14:39 +0300

    обработка ввода данных

--- clock.py
+++ clock.py
@@ -1,8 +1,18 @@
+'''
+Ночь: с 0:00 до 5:59
+Утро: с 6:00 до 11:59
+День: с 12:00 до 17:59
+Вечер: с 18:00 до 23:59
+'''
+
+
 def translate_hours(hours):
-    print()
+    print(hours,end=" ")
+
 
 def translate_minutes(minutes):
-    print()
+    print(minutes,end=" ")
+
 
 def check_hours(hours):
     if hours >= 0 and hours <= 23:
@@ -17,11 +27,37 @@ def check_minutes(minutes):
     else:
         return False
 
-
+input_error_text = "Ошибка, неверный формат вводимых данных!"
 def main():
     ''' обработка ввода чисел'''
+
     print("Введите время в формате:")
     print("часы минуты")
+
+    input_line = input()
+    parts = input_line.split()
+
+    if len(parts) == 2:
+        hours = parts[0]
+        minutes = parts[1]
+
+        if hours.isdigit() and minutes.isdigit():
+            hours = int(hours)
+            minutes = int(minutes)
+
+            if check_hours(hours) and check_minutes(minutes):
+                translate_hours(hours)
+                translate_minutes(minutes)
+            else:
+                print(input_error_text)
+
+        else:
+            print(input_error_text)
+
+    else:
+        print(input_error_text)
+
+
 
 
 
