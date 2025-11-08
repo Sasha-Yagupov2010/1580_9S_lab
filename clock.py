4e3b5b121ce7a07921b951e26f5ce19daaed11f4
Sasha <sasha.yagupov1401@mail.ru>
2025-11-08 16:59:56 +0300

    шаблон

---
+++ clock.py
@@ -0,0 +1,30 @@
+def translate_hours(hours):
+    print()
+
+def translate_minutes(minutes):
+    print()
+
+def check_hours(hours):
+    if hours >= 0 and hours <= 23:
+        return True
+    else:
+        return False
+
+
+def check_minutes(minutes):
+    if minutes >= 0 and minutes <= 59:
+        return True
+    else:
+        return False
+
+
+def main():
+    ''' обработка ввода чисел'''
+    print("Введите время в формате:")
+    print("часы минуты")
+
+
+
+
+if __name__ == "__main__":
+    main()
