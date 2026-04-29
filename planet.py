d7824bd2231ea0d737403bd38b10abed4e948c31
Sasha <sasha.yagupov1401@mail.ru>
2026-04-29 19:00:13 +0300

    remaking

--- planet.py
+++ planet.py
@@ -1,74 +1,80 @@
 import db_driver as db
 
+# planet.py
+import json
+
 class Planet:
-    '''
-    Название планеты
-    Радиус (км)
-    Масса (кг)
-    Расстояние от Солнца (млн км)
-    Тип (каменная, газовый гигант, ледяной гигант)
-    '''
     _id_counter = 0
-    def __init__(self, 
-                 name=None, 
-                 radius=None, 
-                 mass=None,
-                 distance=None,
-                 planet_type=None
-                 ):
-        
+    
+    def __init__(self, name=None, radius=None,  mass=None, distance=None, planet_type=None):
         self.name = name
         self.radius = radius
         self.mass = mass
         self.distance = distance
         self.planet_type = planet_type
 
-
         self.__id = Planet._id_counter
-        Planet._id_counter+=1
-        print(f"Создание ID {id}")
-        
-
-    @staticmethod
-    def get_planet_types():
-        return ["каменная", "газовый гигант", "ледяной гигант"]    
+        Planet._id_counter += 1
+        print(f"Создание ID {self.__id}")
 
 
     def __str__(self):
-        print(f"Планета {self.name}")
+        return (f"Планета '{self.name}' (ID: {self.__id}): "
+                f"Радиус={self.radius} км, Масса={self.mass} кг, "
+                f"Расстояние={self.distance} млн км, Тип='{self.planet_type}'")
 
     def __repr__(self):
-        pass
+        return f"Planet(name='{self.name}', radius={self.radius}, mass={self.mass}, distance={self.distance}, type='{self.planet_type}')"
 
     def __copy__(self):
-        pass
+        return Planet(
+            name=self.name,
+            radius=self.radius,
+            mass=self.mass,
+            distance=self.distance,
+            planet_type=self.planet_type
+        )
 
     def __del__(self):
-        pass
+        print(f"Удаление ID {self.__id}")
 
-    def __lt__(self):
-        pass
+    def __lt__(self, other):
+        return self.distance < other.distance
 
-    def __eq__(self):
-        pass
+    def __eq__(self, other):
+        return self.name == other.name
 
-    def __gt__(self):
-        pass
+    def __gt__(self, other):
+        return self.distance > other.distance
 
-    def __le__(self):
-        pass
+    def __le__(self, other):
+        return self.distance <= other.distance
 
-    def __ge__(self):
-        pass
+    def __ge__(self, other):
+        return self.distance >= other.distance
 
     def to_dict(self):
         return {
-                "name":self.name,
-                "radius": self.radius,
-                "mass":self.mass,
-                "distance": self.distance,
-                "planet_type": self.planet_type 
-                }
+            "name": self.name,
+            "radius": self.radius,
+            "mass": self.mass,
+            "distance": self.distance,
+            "planet_type": self.planet_type 
+        }
+    
+    @staticmethod
+    def get_planet_types():
+        return ["каменная", "газовый гигант", "ледяной гигант"]
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            name=data.get("name"),
+            radius=data.get("radius"),
+            mass=data.get("mass"),
+            distance=data.get("distance"),
+            planet_type=data.get("planet_type")
+        )
 
 def main():
     planet1 = Planet(
@@ -87,21 +93,19 @@ def main():
 
 
 def db_tester():
-    planet1 = Planet(name="del")
-    planet2 = Planet(name="2", mass=54)
-    print(db.load_db())
-    print(db.show_db())
+    planet1 = Planet(
+        name="1", 
+        radius=5621230, 
+        mass=72123213210,
+        distance=150013321230,
+        planet_type="каменная"
+    )
+    planet2 = planet1.__copy__()
+    print(planet2)
     
-    db.add_obj(planet1)
-    print(db.get_object(planet1.name))
-    db.edit_obj(planet2,planet1)
-    print(db.get_object(planet2.name))
-    print(db.get_object(planet1.name))
-    db.del_obj(planet2)
-    print(db.show_db())
 
 
 
 if __name__ == "__main__":
-    main()
+    #main()
     db_tester()
